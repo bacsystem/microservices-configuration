e53b70bece7807d07c5adeba0d3a1e6a0b587db1
cbacilio <dbacilio88@outlook.es>
2025-04-30 22:38:24 -0500

    feat(jenkins): pipeline

--- devops/pipeline/deployment/vars/bacsystem.groovy
+++ devops/pipeline/deployment/vars/bacsystem.groovy
@@ -1,3 +1,47 @@
 def call() {
-    echo "Hello world"
+    pipeline {
+        agent any
+
+        stages {
+            stage('Clonar Repositorio') {
+                steps {
+                    echo 'Clonando el repositorio...'
+                }
+            }
+
+            stage('Construir Proyecto') {
+                steps {
+                    echo 'Construyendo el proyecto...'
+                }
+            }
+
+            stage('Pruebas Unitarias') {
+                steps {
+                    echo 'Ejecutando pruebas unitarias...'
+                }
+            }
+
+            stage('Empaquetar Artefacto') {
+                steps {
+                    echo 'Empaquetando el artefacto...'
+                }
+            }
+
+            stage('Despliegue') {
+                steps {
+                    echo 'Desplegando el artefacto...'
+                }
+            }
+        }
+
+        post {
+            success {
+                echo 'Pipeline completado exitosamente'
+            }
+            failure {
+                echo 'Hubo un error en el pipeline'
+            }
+        }
+    }
+
 }
