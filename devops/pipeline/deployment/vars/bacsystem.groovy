6330ab5a94e4be1032e55cb643648c477295646b
cbacilio <dbacilio88@outlook.es>
2025-04-30 21:37:44 -0500

    feat(jenkins): pipeline

--- devops/pipeline/deployment/vars/bacsystem.groovy
+++ devops/pipeline/deployment/vars/bacsystem.groovy
@@ -1,16 +1,3 @@
-def call(String message) {
-    echo "Hello world: ${message}"
-    pipeline {
-        agent any
-        environment {
-            ENTORNO = 'dev'
-        }
-        stages {
-            stage('Initialize') {
-                steps {
-                    echo "Desplegando en entorno ${ENTORNO}"
-                }
-            }
-        }
-    }
+def call() {
+    echo "Hello world"
 }
