a0dc1a3eb84aee420830b32c0e9143012c6ef619
cbacilio <dbacilio88@outlook.es>
2025-05-02 10:15:53 -0500

    feat(jenkins): pipeline process

--- devops/pipeline/deployment/vars/process.groovy
+++ devops/pipeline/deployment/vars/process.groovy
@@ -4,9 +4,9 @@ import main.flow.constants.IConstants
 def call(Map params = [:]) {
     sh "echo value ${params.value}."
     sh "echo instance ${params.instance}."
-    //sh "echo wout ${params.wout}."
-    //sh "echo smc ${params.smc}."
-    //sh "echo opt ${params.opt}."
+    sh "echo wout ${params.wout}."
+    sh "echo smc ${params.smc}."
+    sh "echo opt ${params.opt}."
 
     def builder = new Builder(params.instance)
     params.builer = builder
