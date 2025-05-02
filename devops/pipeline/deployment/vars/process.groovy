78b5913f5a942971e6e86e6ff4645c6416cbf94b
cbacilio <dbacilio88@outlook.es>
2025-05-02 09:55:31 -0500

    feat(jenkins): pipeline

--- devops/pipeline/deployment/vars/process.groovy
+++ devops/pipeline/deployment/vars/process.groovy
@@ -7,4 +7,16 @@ def call(Map params = [:]) {
     sh "echo smc ${params.smc}."
     sh "echo opt ${params.opt}."
 //String process = IConstants.PipelineProcess.PROCESS.value(), scriptInstance, boolean wout = false, boolean smc = false, boolean opt = false
+    pipeline {
+        agent any
+
+        stages {
+            stage('Build') {
+                steps {
+                    echo "Estoy dentro de un nodo"
+                    sh 'echo hello world'
+                }
+            }
+        }
+    }
 }
