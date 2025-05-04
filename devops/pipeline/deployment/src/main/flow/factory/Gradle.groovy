e53142006ffcf8cf584c146b237022af8d40d197
cbacilio <dbacilio88@outlook.es>
2025-05-03 20:49:50 -0500

    feat(jenkins): pipeline process

--- devops/pipeline/deployment/src/main/flow/factory/Gradle.groovy
+++ devops/pipeline/deployment/src/main/flow/factory/Gradle.groovy
@@ -25,29 +25,44 @@ class Gradle extends BuildFactory {
     void build(def dsl) {
         dsl.COMPILER = "gradle"
         dsl.IS_MAVEN_COMPILER = false
+        dsl.COMPILER_BASE = "sh gradlew"
+        //dsl.env.GRADLE_USER_HOME = "${dsl.env.JENKINS_HOME}/.gradle"
+
         if (!dsl.fileExists("./gradle/wrapper/gradle-wrapper.jar")) {
             dsl.sh "gradle wrapper"
         }
+
         dsl.echo "[INFO] [gradle] Starting the construction of the component."
+        // Prepare gradlew
         def propertyFile = 'properties.tmp'
         dsl.sh "chmod +x ./gradlew"
         dsl.sh "./gradlew -q properties"
         dsl.sh "./gradlew -q properties > ${propertyFile}"
-        String version = readProperties(propertyFile, "version", dsl)
-        String group = readProperties(propertyFile, "group", dsl)
-        String type = readProperties(propertyFile, "type", dsl)
-        String solution = readProperties(propertyFile, "solution", dsl) ? readProperties(propertyFile, "solution", dsl) : readProperties(propertyFile, "app.solution", dsl)
+
+        def readProperties = { file, property ->
+            return dsl.sh(script: "cat ${file} | grep '${property}:' | awk '{print \$2}'", returnStdout: true).trim()
+        }
+
+        String version = readProperties(propertyFile, "version") ?: readProperties(propertyFile, "sdk_version_number")
+        //dsl.env.VERSION_NUM    = dsl.env.VERSION?.replace("-SNAPSHOT", "")
         String name = Utility.repository(dsl)
+        //dsl.env.IMAGE          = dsl.env.APP_NAME
+        //dsl.env.FOLDER = readProperties(propertyFile, 'pipelineFolderModule')
+        String module = readProperties(propertyFile, 'pipelineFolderModule')
+        //dsl.env.PIPE_VERSION   = readProperties(propertyFile,'pipelineVersion')
+        String pipe = readProperties(propertyFile, 'pipelineVersion')
+        String group = readProperties(propertyFile, "group")
+        String type = readProperties(propertyFile, "type")
+        String solution = readProperties(propertyFile, "solution") ?: readProperties(propertyFile, "solution") ?: readProperties(propertyFile, "app.solution")
+        //dsl.env.SONAR_KEY      = "${readProperties(propertyFile,'group')}:${dsl.env.IMAGE}"
 
         dsl.echo "[INFO] [gradle] Version the component. ${version}"
         dsl.echo "[INFO] [gradle] Group the component. ${group}"
         dsl.echo "[INFO] [gradle] Type the component. ${type}"
         dsl.echo "[INFO] [gradle] Solution the component. ${solution}"
         dsl.echo "[INFO] [gradle] Name the component. ${name}"
+        dsl.echo "[INFO] [gradle] Module the component. ${module}"
+        dsl.echo "[INFO] [gradle] Pipe the component. ${pipe}"
         dsl.echo "[INFO] [gradle] Successfully complete the construction of the component."
-    }
-
-    static String readProperties(String file, String property, def dsl) {
-        return dsl.sh(script: "cat ${file} | grep '${property}:' | awk '{print \$2}'", returnStdout: true).trim()
     }
 }
